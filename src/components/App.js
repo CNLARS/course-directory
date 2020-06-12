af0b8b7f804de148da239395b54aa13b0f06a2e2
CNLARS <53753876+CNLARS@users.noreply.github.com>
2020-06-11 20:40:42 -0700

    Semi Finale Edition

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ const App = () => (
       <Header />
 
       <Switch>
-        <Route exact path="/course-directory" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route path="/about" render={ () => <About title="About"/>} />
         <Route exact path="/teachers" component={Teachers} />
         <Route path="/teachers/:topic/:name" component={Featured} />
